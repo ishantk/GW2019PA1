b6d5684d4485cd460c8357d3d7835b5634396a25
Ishant Kumar <er.ishant@gmail.com>
2019-07-04 12:06:22 +0530

    Session24 - Machine Learning - Linear Regression with advertising.csv

--- venv/Session24A.py
+++ venv/Session24A.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+from scipy import stats
+import numpy as np
 
 X = [1, 2, 3, 4, 5]
 Y = [2, 4, 5, 4, 5]
@@ -10,3 +12,29 @@ Y = [2, 4, 5, 4, 5]
     Y = 2.2 + (0.6)X
     ====================== 
 """
+
+data = stats.linregress(X, Y)
+
+print("Slope of Line", data[0])
+print("Intercept of Line", data[1])
+
+
+maxX = np.max(X) + 10
+minX = np.min(X) - 10
+x = np.linspace(minX, maxX, 100)
+y = data[1] + data[0]*x
+
+"""
+print(x)
+print("********")
+print(y)
+"""
+
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.grid(True)
+# plt.plot(X, Y, "o")
+plt.plot(x, y, color="r", label="Regression Line")
+plt.scatter(X, Y, color="b", label="Data Points")
+plt.legend()
+plt.show()
